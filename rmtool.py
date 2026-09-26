16b9bb08d4a2d0ecd72a50589976fa4832228a40
pretenderlu <luqingqin@live.com>
2026-09-26 16:36:25 +0800

    release: v1.16.12 font and plugin mount fixes

--- rmtool.py
+++ rmtool.py
@@ -29,7 +29,7 @@ from PyQt5 import QtCore, QtGui, QtSvg, QtWidgets, sip
 
 
 APP_NAME = "reMarkable 管理工具"
-APP_VERSION = "1.16.11"
+APP_VERSION = "1.16.12"
 CONFIG_FILE = "devices.json"
 GITHUB_REPO_URL = "https://github.com/pretenderlu/rmtool"
 GITHUB_RELEASES_URL = f"{GITHUB_REPO_URL}/releases/latest"
